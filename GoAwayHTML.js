6eab354aff5c8259b79347aac934a773a5405915
BunnyTub <gadiel047@gmail.com>
2026-07-11 00:52:02 -0400

    remove html from path

---
+++ GoAwayHTML.js
@@ -0,0 +1,12 @@
+function RunSteps() {    
+    if (window.location.pathname.endsWith(".html")) {
+        const newPath = window.location.pathname.replace(/\.html$/, "");
+        const isLocal = window.location.protocol === "file:";
+        if (!isLocal) {
+            window.location.replace(newPath);
+            return;
+        }
+    }
+}
+
+RunSteps();
